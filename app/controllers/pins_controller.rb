ee226f70ba8efd65c49a08df6ba8ee909e56ff5a
Sean Pagtulingan <seanpag@me.com>
2014-08-11 19:47:46 -0700

    Up to 50 pins per page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 50)
   end
 
   def show
@@ -14,33 +14,29 @@ class PinsController < ApplicationController
     @pin = current_user.pins.build
   end
 
-
   def edit
   end
-
 
   def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.' 
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render :new 
+      render action: 'new'
     end
   end
-
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.' 
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render :edit 
+      render action: 'edit'
     end
   end
 
-
   def destroy
     @pin.destroy
-    redirect_to pins_url, notice: 'Pin was successfully destroyed.' 
+    redirect_to pins_url
   end
 
   private
@@ -51,11 +47,11 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: 'Not authorized to edit this pin' if @pin.nil?
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-     def pin_params
+    def pin_params
       params.require(:pin).permit(:description, :image)
     end
 end
